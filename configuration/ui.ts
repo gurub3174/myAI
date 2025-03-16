b26e2e233db5c080d2f7010feb789b982b073442
gurub3174 <132667470+gurub3174@users.noreply.github.com>
2025-03-15 21:45:33 -0400

    Update ui.ts with changes to name

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized Credit Card Helper AI by Guru Balamurugan`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${Credit Card Specialist}, ${Guru}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${Credit-Card-Specialist}, ${Guru}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
